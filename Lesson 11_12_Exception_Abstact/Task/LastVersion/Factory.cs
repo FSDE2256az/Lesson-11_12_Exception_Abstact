a4468b2b59303969afa39bdd8baa3b103cec281e
karimzade_k <karimzade_k@STHQ012D-01>
2026-06-14 15:03:52 +0400

    last version started

---
+++ Lesson 11_12_Exception_Abstact/Task/LastVersion/Factory.cs
@@ -0,0 +1,95 @@
+﻿namespace Lesson_11_12_Exception_Abstact.Task.LastVersion;
+
+
+abstract class Sofa { };
+abstract class CoffeeTable {  };
+abstract class Chair {  };
+
+
+class VictorianSofa : Sofa
+{
+    public VictorianSofa()
+    {
+        Console.WriteLine("VictorianSofa");
+    }
+}
+
+class ModernSofa : Sofa
+{
+    public ModernSofa()
+    {
+        Console.WriteLine("ModernSofa");
+    }
+}
+
+class ArtDecoSofa: Sofa
+{
+    public ArtDecoSofa()
+    {
+        Console.WriteLine("ArtDecoSofa");
+    }
+}
+
+
+class VictorianCoffeeTable : CoffeeTable
+{
+    public VictorianCoffeeTable()
+    {
+        Console.WriteLine("VictorianCoffeeTable");
+    }
+}
+
+class ModernCoffeeTable : CoffeeTable
+{
+    public ModernCoffeeTable()
+    {
+        Console.WriteLine("ModernCoffeeTable");
+    }
+}
+
+class ArtDecoCoffeeTable : CoffeeTable
+{
+    public ArtDecoCoffeeTable()
+    {
+        Console.WriteLine("ArtDecoCoffeeTable");
+    }
+}
+
+
+class VictorianChair: Chair
+{
+    public VictorianChair()
+    {
+        Console.WriteLine("VictorianChair");
+    }
+}
+
+class ModernChair : Chair
+{
+    public ModernChair()
+    {
+        Console.WriteLine("ModernChair");
+    }
+}
+
+class ArtDecoChair : Chair
+{
+    public ArtDecoChair()
+    {
+        Console.WriteLine("ArtDecoChair");
+    }
+}
+
+
+
+
+
+
+public class Factory
+{
+    public Sofa CreateSofa();
+    public Chair CreateChair() ;
+    public abstract CoffeeTable CreateCoffeeTable();
+
+
+}
