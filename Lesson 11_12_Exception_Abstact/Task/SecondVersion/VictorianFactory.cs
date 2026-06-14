0c776212ff08635924d75d27854ed934b23d79e6
karimzade_k <karimzade_k@STHQ012D-01>
2026-06-14 14:50:58 +0400

    third version uncompleted

---
+++ Lesson 11_12_Exception_Abstact/Task/SecondVersion/VictorianFactory.cs
@@ -0,0 +1,32 @@
+﻿using Lesson_11_12_Exception_Abstact.Task.FirstVersion;
+
+namespace Lesson_11_12_Exception_Abstact.Task.SecondVersion;
+
+public class VictorianFactory
+{
+    public VictorianSofa CreateVictorianSofa() => new VictorianSofa();
+    public VictorianChair CreateVictorianChair() => new VictorianChair();
+    public VictorianCofeeTable CreateVictorianCofeeTable() => new VictorianCofeeTable();
+
+    public ModernSofa CreateModernSofa() => new ModernSofa();
+    public ModernChair CreateModernChair() => new ModernChair();
+    public ModernCoffeeTable CreateCoffeeTable() => new ModernCoffeeTable();
+
+    public ArtDecoSofa CreateArtDecoSofa() => new ArtDecoSofa();
+    public ArtDecoChair CreateArtDecoChair() => new ArtDecoChair();
+    public ArtDecoCoffeeTable CreateArtDecoCoffeeTable() => new ArtDecoCoffeeTable();
+
+
+}
+
+public class VictorianSofa { }
+public class VictorianChair { }
+public class VictorianCoffeeTable { }
+
+public class ModernSofa { }
+public class ModernChair { }
+public class ModernCoffeeTable { }
+
+public class ArtDecoSofa { }
+public class ArtDecoChair { }
+public class ArtDecoCoffeeTable { }
---
+++ Lesson 11_12_Exception_Abstact/Task/ThirdVersion/VictorianFactory.cs
@@ -0,0 +1,55 @@
+﻿using Lesson_11_12_Exception_Abstact.Task.FirstVersion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_11_12_Exception_Abstact.Task.ThirdVersion;
+
+
+public class VictorianFactory
+{
+    public string ModelName { get; set; }
+
+    public VictorianFactory(string modelName)
+    {
+        ModelName = modelName;
+    }
+
+    public object CreateSofa()
+    {
+        if (ModelName == "Victorian")
+            return new VictorianSofa();
+
+        else if (ModelName == "ArtDeco")
+            return new ArtDecoSofa();
+
+        else
+            return new ModernSofa();
+
+    }
+
+    public object CreateChair()
+    {
+
+    }
+
+    public object CreateCofeeTable()
+    {
+
+    }
+
+}
+
+public class VictorianSofa { }
+public class VictorianChair { }
+public class VictorianCoffeeTable { }
+
+public class ModernSofa { }
+public class ModernChair { }
+public class ModernCoffeeTable { }
+
+public class ArtDecoSofa { }
+public class ArtDecoChair { }
+public class ArtDecoCoffeeTable { }
